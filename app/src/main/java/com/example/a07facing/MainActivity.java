2256b71b5fd6cb324aee76fc47bdcb2a87e42a24
Huda Al-Regib <73912953+hudz1234@users.noreply.github.com>
2023-02-09 13:47:35 -0500

    Update MainActivity.java
    added the rest of the methods not completed

--- app/src/main/java/com/example/a07facing/MainActivity.java
+++ app/src/main/java/com/example/a07facing/MainActivity.java
@@ -1,73 +1,96 @@
 package com.example.a07facing;
-import android.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends Activity implements SensorEventListener{
+import java.util.Locale;
 
-    // define the display assembly compass picture
-    Button button2 = findViewById(R.id.button2);
-    private ImageView image;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    Sensor accelerometer;
+    Sensor magnetometer;
+    private String direction;
+    private String dir[] = {"North", "Northeast", "East", "Southeast", "South", "Southwest", "West", "Northwest", "North"};
 
-    // record the compass picture angle turned
+    // for text to speech
+    private TextToSpeech tts;
+    private ImageView imageview = (ImageView) findViewById(R.id.imageView);
+    private TextView textView;
+
     private float currentDegree = 0f;
-
-    // device sensor manager
-    private SensorManager mSensorManager;
-
-    TextView tvHeading;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // imageview click callback
+        imageview.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick (View arg0){
+                TextView textview = (TextView) findViewById(R.id.textView);
+                textview.setText("Image is clicked");
+            }
+        });
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = tts.setLanguage(Locale.US);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    } else {
+                    }
+
+                } else {
+                }
+            }
+        });
         setContentView(R.layout.activity_main);
-
-        // our compass image
-        image = (ImageView) findViewById(R.id.imageViewCompass);
-
-        // TextView that will tell the user what degree is he heading
-        tvHeading = (TextView) findViewById(R.id.tvHeading);
-
-        // initialize your android device sensor capabilities
-        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View imageview){
+                speakOut();
+                rotate();
+            }
+        });
     }
-
     @Override
-    protected void onResume() {
+            protected void onResume() {
         super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
 
-        // for the system's orientation sensor registered listeners
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-                SensorManager.SENSOR_DELAY_GAME);
     }
-
     @Override
-    protected void onPause() {
+    protected void onPause(){
         super.onPause();
-
-        // to stop the listener and save battery
-        mSensorManager.unregisterListener(this);
+        sensorManager.unregisterListener(this);
     }
-
     @Override
     public void onSensorChanged(SensorEvent event) {
-
         // get the angle around the z-axis rotated
         float degree = Math.round(event.values[0]);
 
-        tvHeading.setText("Heading: " + Float.toString(degree) + " degrees");
+        textView.setText("Heading: " + Float.toString(degree) + " degrees");
 
         // create a rotation animation (reverse turn degree degrees)
         RotateAnimation ra = new RotateAnimation(
@@ -84,23 +107,35 @@ public class MainActivity extends Activity implements SensorEventListener{
         ra.setFillAfter(true);
 
         // Start the animation
-        image.startAnimation(ra);
+        imageview.startAnimation(ra);
         currentDegree = -degree;
-
     }
-
-    // tried adding button to change bg color, getting some errors.
-
-//    button2.setOnClickListener(new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//            // set the color to relative layout
-//            image.setBackgroundColor(R.color.purple_500);
-//        }
-//    });
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // not in use
+
     }
+
+    // my function for image rotation
+// use a fixed value (45 degrees) here for later expansion
+    private void rotate() {
+
+        //// As assignment
+    }
+    @Override
+    public void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    private void speakOut() {
+        String text = "You are facing " + direction;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(text,TextToSpeech.QUEUE_FLUSH,null,null);
+        } else {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }    }
 }
